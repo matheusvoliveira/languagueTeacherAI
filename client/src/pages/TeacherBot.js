ae2a9b3853ebd47d81e9c174e8f71e00fafbacdf
Matheus Soares <100249571+matheusvoliveira@users.noreply.github.com>
2024-09-17 11:27:06 -0300

    Update TeacherBot.js

--- client/src/pages/TeacherBot.js
+++ client/src/pages/TeacherBot.js
@@ -8,7 +8,7 @@ import ChatMessage from "./ChatMessage";
 
 const TeacherBot = () => {
   const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: process.env.API_URL,
   });
   const [input, setInput] = useState("");
   const [chatLog, setChatLog] = useState([
